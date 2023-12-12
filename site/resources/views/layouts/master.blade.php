09a3a2f0389ca3050c8580c26905229383bbe131
Brian Tobias <brian.tobias.arrua.dominguez@alumnat.copernic.cat>
2023-12-12 19:56:46 +0100

    Añadimos el logo del projecto,tambiem ponemos una ruta por defecto i creamos la pagina home y la pagina master donde esta el footer y el header provisional,creamos el nombre del projecto y tambien he creado el style.css donde estar los estilos por defectos

---
+++ site/resources/views/layouts/master.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="ca"> 
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="{{ asset('imagen/logo-definitivo.ico') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}"> 
+
+    <title>@yield('title')</title> 
+</head>
+
+<body class="w3-content">
+  <header>
+    <div class="divHeader">
+      <h1 class="titulo">ShowTickt</h1>
+      <img class="logo" src="{{ asset('imagen/logo-definitivo.png') }}">
+    </div>
+    
+  </header>
+  <div>
+    @yield('content') 
+  </div>
+  <footer>
+
+  </footer>
+</body>
+
+</html>

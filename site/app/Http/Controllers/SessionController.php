b6269a19d844216262b12b81eff777c1937502e2
Brian Tobias <brian.tobias.arrua.dominguez@alumnat.copernic.cat>
2024-01-11 20:05:34 +0100

    Acabamos el controlador de la secion donde estan las funciones de cerrar session, iniciar y el perfil, tambien creacoms el fatory de las fechas con su seeder y modelo

--- site/app/Http/Controllers/SessionController.php
+++ site/app/Http/Controllers/SessionController.php
@@ -6,18 +6,32 @@ use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
-  public function SessionController(Request $request){
-    var_dump ($request->input('sesion'));
+  public function out(){
+    session()->forget('key');
+    return redirect('login');
   }
-    public function out(){
-      session()->forget('key');
-      session_destroy();
-      return view('login');
+  public function profile(){
+    return view('perfil');
+  }
+  public function in(){
+    return view('login');
+  }
+
+  public function SessionController(Request $request){
+    $session=$request->input('sesionOpcion');
+    switch ($session){
+      case "profile":
+        $this->profile();
+        return view('perfil');
+        break;
+    case "closeSession":
+        $this->out();
+        return view('login');
+        break;
+    case "openSession":
+        $this->in();
+        return view('login');
+        break;
     }
-    public function profile(){
-      return view('perfil');
-    }
-    public function in(){
-      return view('login');
-    }
+  }
 }

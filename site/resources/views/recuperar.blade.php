e1f944020bd63431e1ed6584fe34c0a4e5a98d67
cjumelle <cjumelle@alumnat.copernic.cat>
2024-01-11 20:10:59 +0100

    Controller y viewer de la pestaña olvidar contraseña

---
+++ site/resources/views/recuperar.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.master')
+
+@section('title', 'Recuperar')
+
+@section('content')
+<div class="loginPage">
+    <div class="login">
+        <h2>Contraseña Olvidada</h2>
+        @if($errors->has('msg'))
+            <span class="errorMsg">El email no esta vinculado con ninguno de nuestros usuarios.</span>
+        @endif
+        
+        <span>Escriba el email de la cuenta a recuperar.</span> <br> <br>
+        <form action="{{route('homePromotor')}}" method="post" id="loginForm">
+        @csrf
+            <div class="loginInput">
+                <input type="email" name="email" id="email" placeholder="Email">
+            </div>
+            <button class="boton" id="atras">Atrás</button>
+            <input type="submit" value="Acceder" class="boton">
+        </form>
+    </div>
+</div>
+@endsection
--- site/routes/web.php
+++ site/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\EsdevenimentController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\passwordController;
+use App\Http\Controllers\PasswordController as ControllersPasswordController;
 use App\Http\Controllers\SessionController;
 
 /*
@@ -25,3 +27,4 @@ Route::post('/login',[LoginController::class,'login'])->name(('login'));
 Route::post('/homePromotor', [LoginController::class,'iniciarSesion'])->name('homePromotor');
 Route::post('/perfil', [LoginController::class,'iniciarSesion'])->name('perfil');
 Route::get('/session', [SessionController::class, 'SessionController'])->name('session');
+Route::get('/recuperar',[PasswordController::class,'passwordPage'])->name('recuperar');

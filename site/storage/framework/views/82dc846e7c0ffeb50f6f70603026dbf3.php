693c1f48e7f194e80ad8ab980207cd7fec5573c6
cjumelle <cjumelle@alumnat.copernic.cat>
2024-01-18 19:22:47 +0100

    Corregido login controller, si no pones un campo t avisa. Y actualizada la memoria en el seguimiento semanal

---
+++ site/storage/framework/views/82dc846e7c0ffeb50f6f70603026dbf3.php
@@ -0,0 +1,21 @@
+<?php $__env->startSection('title', 'resultados'); ?>
+
+<?php $__env->startSection('content'); ?>
+    <div class="event-cards">
+        <?php $__currentLoopData = $esdeveniments; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $esdeveniment): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <a href="<?php echo e(route('editar-esdeveniment', ['id' => $esdeveniment->id])); ?>" class="event-link">
+                <div class="event-card">
+                    <div class="event-details">
+                        <p><?php echo e($esdeveniment->nom); ?>  </p>
+                        <p><?php echo e($esdeveniment->data_sessio); ?></p>
+                        <p><?php echo e($esdeveniment->recinte->lloc); ?></p>
+                    </div>
+                    <img src="<?php echo e(asset($esdeveniment->imatge)); ?>" alt="Imatge de l'esdeveniment">
+                </div>
+            </a>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </div>
+    <div class="pages"><?php echo e($esdeveniments->links('pagination::bootstrap-5')); ?></div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\jumel\OneDrive\Escritorio\gr6-arrua-galindo-jumelle\site\resources\views/administrarEsdeveniments.blade.php ENDPATH**/ ?>

ae14815e7577d20e02d639c4155342d56a2d1f59
Joan Galindo <galindo.palacio.joan@alumnat.copernic.cat>
2024-01-26 17:10:13 +0100

    ordenar eventos por fecha en administraresdeveniments, no mostrar los anteriores a la fecha actual

--- site/app/Http/Controllers/AdministrarEsdevenimentsController.php
+++ site/app/Http/Controllers/AdministrarEsdevenimentsController.php
@@ -20,9 +20,26 @@ class AdministrarEsdevenimentsController extends Controller
         $userId = $request->session()->get('user_id');
 
         // Obtén los eventos asociados al usuario con sus sesiones (usando with)
-        $esdeveniments = Esdeveniment::with('sesions')->where('user_id', $userId)->paginate(6);
+        //$esdeveniments = Esdeveniment::with('sesions')->where('user_id', $userId)->paginate(6);
+
+        // Obtén los eventos asociados al usuario con sus sesiones (usando with)
+        $query = Esdeveniment::with(['recinte'])
+            ->join(
+                DB::raw('(SELECT esdeveniments_id, MIN(data) as min_data FROM sessios GROUP BY esdeveniments_id) as min_dates'),
+                function ($join) {
+                    $join->on('esdeveniments.id', '=', 'min_dates.esdeveniments_id');
+                }
+            )
+            ->leftJoin('sessios', function ($join) {
+                $join->on('esdeveniments.id', '=', 'sessios.esdeveniments_id')
+                    ->on('sessios.data', '=', 'min_dates.min_data');
+            })
+            ->select('esdeveniments.*', 'min_dates.min_data as min_data')
+            ->where('min_dates.min_data', '>', now()) // Filtrar eventos cuya fecha mínima sea mayor que la fecha local actual
+            ->orderBy('min_data', 'asc') // Ordenar por fecha mínima de sesión ascendente
+            ->paginate(6); // Puedes usar ->paginate($pag) si deseas paginación
 
         // Pasa los eventos a la vista
-        return view('administrarEsdeveniments', ['esdeveniments' => $esdeveniments]);
+        return view('administrarEsdeveniments', ['esdeveniments' => $query]);
     }
 }

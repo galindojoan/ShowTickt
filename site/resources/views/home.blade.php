0698d021e5856af793ecc78c026c379561e8ed1a
Joan Galindo <galindo.palacio.joan@alumnat.copernic.cat>
2024-02-05 17:22:14 +0100

    crear componente de tarjeta  de evento

--- site/resources/views/home.blade.php
+++ site/resources/views/home.blade.php
@@ -68,23 +68,8 @@
                             $cont++;
                         @endphp
                         <a href="{{ route('mostrar-esdeveniment', ['id' => $esdeveniment->id]) }}" class="event-link">
-                            <div class="event-card">
-                                <div class="event-details">
-                                    <h1>{{ $esdeveniment->nom }}</h1>
-                                    @if ($esdeveniment->sesions->isNotEmpty() && $esdeveniment->sesions->first()->data !== null)
-                                        <h3>{{ $esdeveniment->sesions->first()->data }}</h3>
-                                    @else
-                                    <h3>No hay sesiones</h3>
-                                    @endif
-                                        <h4>{{ $esdeveniment->recinte->lloc }}</h4>
-                                        @if ($esdeveniment->sesions->isNotEmpty() && $esdeveniment->sesions->first()->entrades->isNotEmpty())
-                                            <h2>{{ $esdeveniment->sesions->first()->entrades->first()->preu }} €</h2>
-                                        @else
-                                        <h2>Entradas Agotadas</h2>
-                                        @endif
-                                </div>
-                                <img src="{{ Storage::url($esdeveniment->imatge) }}" alt="Imatge de l'esdeveniment">
-                            </div>
+                            {{-- Incluir el componente de tarjeta de evento --}}
+                            @include('components.event-card', ['esdeveniment' => $esdeveniment])
                         </a>
                     @endif
                 @endforeach
--- site/storage/framework/views/8aa1b598214feb1376d1f8d5ab198435.php
+++ site/storage/framework/views/8aa1b598214feb1376d1f8d5ab198435.php
@@ -56,23 +56,8 @@
         <div class="event-cards">
             <?php $__currentLoopData = $esdeveniments; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $esdeveniment): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                 <a href="<?php echo e(route('mostrar-esdeveniment', ['id' => $esdeveniment->id])); ?>" class="event-link">
-                    <div class="event-card">
-                        <div class="event-details">
-                            <h1><?php echo e($esdeveniment->nom); ?> </h1>
-                            <?php if($esdeveniment->sesions->isNotEmpty() && $esdeveniment->sesions->first()->data !== null): ?>
-                                <h3><?php echo e($esdeveniment->sesions->first()->data); ?></h3>
-                            <?php else: ?>
-                                <h3>No hay sesiones</h3>
-                            <?php endif; ?>
-                            <h4><?php echo e($esdeveniment->recinte->lloc); ?></h4>
-                            <?php if($esdeveniment->sesions->isNotEmpty() && $esdeveniment->sesions->first()->entrades->isNotEmpty()): ?>
-                                <h2><?php echo e($esdeveniment->sesions->first()->entrades->first()->preu); ?> €</h2>
-                            <?php else: ?>
-                                <h2>Entradas Agotadas</h2>
-                            <?php endif; ?>
-                        </div>
-                        <img src="<?php echo e(Storage::url($esdeveniment->imatge)); ?>" alt="Imatge de l'esdeveniment">
-                    </div>
+                    
+                    <?php echo $__env->make('components.event-card', ['esdeveniment' => $esdeveniment], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
                 </a>
             <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
         </div>
